a424043e5d3bb5aaeb07963773afa89b698f4d2a
Pindulfo <guillermofuente06@gmail.com>
2024-10-16 20:35:19 +0200

    datos muertes diabetes cargados

--- seminario.R
+++ seminario.R
@@ -7,9 +7,9 @@ archivos <- list.files('INPUT/DATA/Diabetes/Ingresos/', pattern = '*.px')
 archivos_pc <- sapply(archivos, function(x) paste0('INPUT/DATA/Diabetes/Ingresos/',x))
 
 #crea un dataframe con el primer archivo que sevirá de molde para añadir los siguientes años
-df <- data.frame(as.data.frame(read.px(archivos_pc[1])))
+df_i <- data.frame(as.data.frame(read.px(archivos_pc[1])))
 #añade el atributo del año de los datos
-df['Año'] = 1997
+df_i['Año'] = 1997
 
 for (i in 2:length(archivos_pc)){
   #genera un df con los datos del año
@@ -23,15 +23,20 @@ for (i in 2:length(archivos_pc)){
   df_provisional['Año'] = año
   
   #hace que los nombres de las columnas sean iguales al dataframe finak
-  colnames(df_provisional) <- colnames(df)
+  colnames(df_provisional) <- colnames(df_i)
   
   #une el dataframe con los datos del año al df final
-  df <- rbind(df,df_provisional)
+  df_i <- rbind(df_i,df_provisional)
 }
 
 #dataframe con todos los datos de todos los años
-df
+#df_i
+head(df_i)
 
 
 
-
+#Lo mismo para los datos de muertes
+datos_muertes <- read.px('INPUT/DATA/Diabetes/Muertes/muertes1997-2022.px')
+datos_muertes
+df_m <- as.data.frame(datos_muertes)
+head(df_m)
