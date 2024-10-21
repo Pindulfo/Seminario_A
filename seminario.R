c042cead7ccf93c5b46e083d3c78f7deadb852fe
Pindulfo <guillermofuente06@gmail.com>
2024-10-21 09:59:23 +0200

    recuperar código perdido

--- seminario.R
+++ seminario.R
@@ -4,6 +4,13 @@ library(pxR)
 library(climaemet)
 
 
+<<<<<<< HEAD
+=======
+#crea un dataframe con el primer archivo que sevirá de molde para añadir los siguientes años
+df <- data.frame(as.data.frame(read.px(archivos_pc[1])))
+#añade el atributo del año de los datos
+df['Año'] = 1997
+>>>>>>> parent of a424043 (datos muertes diabetes cargados)
 
 ## Use this function to register your API Key temporarly or permanently
 aemet_api_key("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcGFibG81NDVAZ21haWwuY29tIiwianRpIjoiYTI2NWIyNzQtY2M4OS00NWZmLThlNGYtMWJlYWQ2NTA1MTAxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3MjkzNTMxNTgsInVzZXJJZCI6ImEyNjViMjc0LWNjODktNDVmZi04ZTRmLTFiZWFkNjUwNTEwMSIsInJvbGUiOiIifQ.TGFw-QlkAkMyQ2hItIpbSF_xDIoN42JpIzUhf-dOm3A", install= TRUE)
@@ -39,6 +46,7 @@ for (i in 1:nrow(estaciones_unicas_provincia)) {
   # Obtener el código de cada estación (indicativo)
   codigo_estacion <- estaciones_unicas_provincia[i, "indicativo"]
   
+<<<<<<< HEAD
   # Intentar obtener los datos meteorológicos recientes de la estación
   tryCatch({
     datos_clima <- aemet_last_obs(codigo_estacion)
@@ -58,6 +66,20 @@ for (i in 1:nrow(estaciones_unicas_provincia)) {
 # Ver los datos obtenidos de las estaciones únicas por provincia
 head(datos_unica_estacion_por_provincia)
 
+=======
+  #genera una columna con el año de los datos
+  df_provisional['Año'] = año
+  
+  #hace que los nombres de las columnas sean iguales al dataframe finak
+  colnames(df_provisional) <- colnames(df)
+  
+  #une el dataframe con los datos del año al df final
+  df <- rbind(df,df_provisional)
+}
+
+#dataframe con todos los datos de todos los años
+df
+>>>>>>> parent of a424043 (datos muertes diabetes cargados)
 
 
 
