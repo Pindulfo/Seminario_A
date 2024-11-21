31ccfe70caa6b73ec30f7db5a4a4fb9e71849b53
pfg1008 <pfg1008@alu.ubu.es>
2024-11-21 19:09:34 +0100

    Añado algun grafico de prueba más

--- Pruebas_Varias.R
+++ Pruebas_Varias.R
@@ -13,263 +13,7 @@ library(plotly)
 ## Use this function to register your API Key temporarly or permanently
 aemet_api_key("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcGFibG81NDVAZ21haWwuY29tIiwianRpIjoiYTI2NWIyNzQtY2M4OS00NWZmLThlNGYtMWJlYWQ2NTA1MTAxIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3MjkzNTMxNTgsInVzZXJJZCI6ImEyNjViMjc0LWNjODktNDVmZi04ZTRmLTFiZWFkNjUwNTEwMSIsInJvbGUiOiIifQ.TGFw-QlkAkMyQ2hItIpbSF_xDIoN42JpIzUhf-dOm3A", install= TRUE)
 
-#-----------------------------------------
-# DATOS AEMET
-#-----------------------------------------
 
-#codigo para extrar todos los datos climaticos de las estaciones de la aemet
-estaciones <- aemet_stations()
-
-estaciones <- estaciones %>% 
-  select(indicativo,provincia) %>% 
-  rename(codigo = indicativo)
-typeof(datos_met$codigo)
-typeof(estaciones$codigo)
-
-datos_met <- merge(datos_met,estaciones, by = 'codigo')
-
-#obtener las estaciones principales (varias por provincia)
-estaciones_ppales <- dplyr::filter(.data = estaciones, nchar(indsinop) > 2)
-provincias <- attributes(table(estaciones_ppales$provincia))$dimnames[[1]]
-provincias <- provincias[provincias !='BALEARES']
-
-#obtiene las estaciones que contienen el nombre de la provincia
-cosa2 <- dplyr::filter(.data = estaciones_ppales,  mapply(grepl, estaciones_ppales$provincia, estaciones_ppales$nombre))
-
-#códigos de estaciones que no interesan
-eliminar <- c('C659M', '0201D', '1387D', '1387E', '2867', '3168C', '8175', '8019', '5514', '5514Z', '3168', '3168Z', '3194', '3194U', '3195', '3196', '2539', '9434C', '9434P')
-for (i in eliminar){
-  cosa2 <- cosa2[!(cosa2$indicativo == i),]
-}
-resumen <- factor(cosa2$provincia, levels = provincias)
-table(resumen)
-
-#ver las que faltan para añadirlas a mano
-añadir <- c('6325O', '9091R', '2444', 'B278', '1082', '3469A', '5973', '1111', '8500A', '5402', '1014A', '5270B', '9170', '6156X', '9263D',' 2374X', '0016A', '2661')
-for (i in añadir){
-  cosa2 <- rbind(cosa2,estaciones_ppales[estaciones_ppales$indicativo == i,])
-}
-resumen <- factor(cosa2$provincia, levels = provincias)
-table(resumen)
-cosa2 <- rbind(cosa2, estaciones_ppales[estaciones_ppales$indicativo == '2235U',])
-resumen <- factor(cosa2$provincia, levels = provincias)
-table(resumen)
-
-#OBTENER LOS CÓDIGOS DE LAS ESTACIONES
-codigos <- cosa2$indicativo
-datos_met <- data.frame(matrix(ncol = 5))
-colnames(datos_met) <- c('codigo', 'ta_max', 'ta_min', 'tm_mes', 'año')
-met_22 <- as.data.frame(aemet_monthly_clim('B278', year = 1990)) %>%
-  slice (1:12) %>%
-  select(c('ta_max', 'ta_min', 'tm_mes'))
-
-
-met_22$ta_max <- sapply(met_22$ta_max, FUN = function(x) unlist(strsplit(x, '\\('))[1] )
-met_22$ta_min <- sapply(met_22$ta_min, FUN = function(x) unlist(strsplit(x, '\\('))[1] )
-
-for (año in 2017:2020){
-  print(año)
-  for (i in codigos){
-    print(i)
-    met_prov <- as.data.frame(aemet_monthly_clim(i, year = año)) %>% 
-      slice (1:12) %>% 
-      select(c('ta_max', 'ta_min', 'tm_mes'))
-    
-    met_prov$ta_max <- sapply(met_prov$ta_max, FUN = function(x) as.numeric(unlist(strsplit(x, '\\('))[1]) )
-    met_prov$ta_min <- sapply(met_prov$ta_min, FUN = function(x) as.numeric(unlist(strsplit(x, '\\('))[1]) )
-    
-    
-    met_procesados <-  data.frame('codigo' = i,
-                                  'ta_max' = max(met_prov$ta_max, na.rm = TRUE), 
-                                  'ta_min' = min(met_prov$ta_min, na.rm = TRUE), 
-                                  'tm_mes' = mean(met_prov$tm_mes, na.rm = TRUE),
-                                  'año' = año)
-    # print(met_procesados)
-    datos_met <- rbind(datos_met, met_procesados)
-    # print(datos_met)
-    Sys.sleep(2)
-  }
-}
-
-save(datos_met,file = './datos_aemet.RData')
-
-#-------------------------------------------------------
-#Modificacion del dataframe de datos_aemet
-#-------------------------------------------------------
-
-#Carga de archivos
-load(file = 'objetos.RData')
-load(file = 'datos_aemet.RData')
-cod_estaciones <- estaciones %>% 
-  select(indicativo,provincia) %>% 
-  rename(codigo = indicativo)
-typeof(datos_met$codigo)
-typeof(cod_estaciones$codigo)
-#OBTENER LOS CÓDIGOS DE LAS ESTACIONES
-codigos <- cosa2$indicativo
-datos_met <- data.frame(matrix(ncol = 5))
-colnames(datos_met) <- c('codigo', 'ta_max', 'ta_min', 'tm_mes', 'año')
-met_22 <- as.data.frame(aemet_monthly_clim('B278', year = 1990)) %>%
-  slice (1:12) %>%
-  select(c('ta_max', 'ta_min', 'tm_mes'))
-
-
-met_22$ta_max <- sapply(met_22$ta_max, FUN = function(x) unlist(strsplit(x, '\\('))[1] )
-met_22$ta_min <- sapply(met_22$ta_min, FUN = function(x) unlist(strsplit(x, '\\('))[1] )
-
-for (año in 2017:2020){
-  print(año)
-  for (i in codigos){
-    print(i)
-    met_prov <- as.data.frame(aemet_monthly_clim(i, year = año)) %>% 
-      slice (1:12) %>% 
-      select(c('ta_max', 'ta_min', 'tm_mes'))
-    
-    met_prov$ta_max <- sapply(met_prov$ta_max, FUN = function(x) as.numeric(unlist(strsplit(x, '\\('))[1]) )
-    met_prov$ta_min <- sapply(met_prov$ta_min, FUN = function(x) as.numeric(unlist(strsplit(x, '\\('))[1]) )
-    
-    
-    met_procesados <-  data.frame('codigo' = i,
-                                  'ta_max' = max(met_prov$ta_max, na.rm = TRUE), 
-                                  'ta_min' = min(met_prov$ta_min, na.rm = TRUE), 
-                                  'tm_mes' = mean(met_prov$tm_mes, na.rm = TRUE),
-                                  'año' = año)
-    # print(met_procesados)
-    datos_met <- rbind(datos_met, met_procesados)
-    # print(datos_met)
-    Sys.sleep(2)
-  }
-}
-
-save(datos_met,file = './datos_aemet.RData')
-
-#-------------------------------------------------------
-#Modificacion del dataframe de datos_aemet
-#-------------------------------------------------------
-
-#Carga de archivos
-load(file = 'objetos.RData')
-load(file = 'datos_aemet.RData')
-cod_estaciones <- estaciones %>% 
-  select(indicativo,provincia) %>% 
-  rename(codigo = indicativo)
-typeof(datos_met$codigo)
-typeof(cod_estaciones$codigo)
-
-#Union de datos_met y cod_estaciones
-datos_met <- merge(datos_met,cod_estaciones, by = 'codigo')
-
-#Modificacion de datos y columnas para tener los mismos valores en todos los df
-datos_met<-rename(.data = datos_met, Provincia = provincia, Año = año)
-datos_met <- select(datos_met,-codigo)
-datos_met$Año <- as.character(datos_met$Año)
-
-datos_met$Provincia <- str_replace_all(datos_met$Provincia,"TARRAGONA","Tarragona") %>% 
-  str_replace_all("BARCELONA", "Barcelona") %>% 
-  str_replace_all("GIRONA", "Girona") %>% 
-  str_replace_all("GIPUZKOA", "Gipuzkoa") %>% 
-  str_replace_all("BIZKAIA", "Vizcaya") %>% 
-  str_replace_all("CANTABRIA", 'Cantabria') %>% 
-  str_replace_all('ASTURIAS','Asturias') %>% 
-  str_replace_all("A CORUÑA", "A Coruña") %>% 
-  str_replace_all("PONTEVEDRA", "Pontevedra") %>% 
-  str_replace_all("LUGO", "Lugo") %>% 
-  str_replace_all("OURENSE", "Ourense") %>% 
-  str_replace_all("SORIA", "Soria") %>% 
-  str_replace_all("PALENCIA", "Palencia") %>% 
-  str_replace_all("BURGOS", "Burgos") %>% 
-  str_replace_all("VALLADOLID", "Valladolid") %>% 
-  str_replace_all("AVILA", "Avila") %>% 
-  str_replace_all("SEGOVIA", "Segovia") %>% 
-  str_replace_all("ZAMORA", "Zamora") %>% 
-  str_replace_all("LEON", "León") %>%
-  str_replace_all("SALAMANCA", "Salamanca") %>% 
-  str_replace_all("GUADALAJARA", "Guadalajara") %>% 
-  str_replace_all("MADRID", "Madrid") %>% 
-  str_replace_all("TOLEDO", "Toledo") %>% 
-  str_replace_all("CACERES", "Cáceres") %>% 
-  str_replace_all("CIUDAD REAL", "Ciudad Real") %>% 
-  str_replace_all("BADAJOZ", "Badajoz") %>% 
-  str_replace_all("HUELVA", "Huelva") %>% 
-  str_replace_all("CEUTA", "Ceuta") %>% 
-  str_replace_all("JAEN", "Jaén") %>% 
-  str_replace_all("CORDOBA", "Córdoba") %>% 
-  str_replace_all("GRANADA", "Granada") %>% 
-  str_replace_all("SEVILLA", "Sevilla") %>% 
-  str_replace_all("CADIZ", "Cádiz") %>% 
-  str_replace_all("MELILLA", "Melilla") %>% 
-  str_replace_all("MALAGA", "Málaga") %>% 
-  str_replace_all("ALMERIA", "Almería") %>% 
-  str_replace_all("MURCIA", "Murcia") %>% 
-  str_replace_all("ALICANTE", "Alicante") %>% 
-  str_replace_all("CUENCA", "Cuenca") %>% 
-  str_replace_all("ALBACETE", "Albacete") %>% 
-  str_replace_all("TERUEL", "Teruel") %>% 
-  str_replace_all("VALENCIA", "Valencia") %>% 
-  str_replace_all("CASTELLON", "Castellón") %>% 
-  str_replace_all("ARABA/ALAVA", "Alava") %>% 
-  str_replace_all("LA RIOJA", "La Rioja") %>% 
-  str_replace_all("NAVARRA", "Navarra") %>% 
-  str_replace_all("ZARAGOZA", "Zaragoza") %>% 
-  str_replace_all("LLEIDA", "Lleida") %>% 
-  str_replace_all("HUESCA", "Huesca") %>% 
-  str_replace_all("ILLES BALEARS", "Baleares") %>% 
-  str_replace_all("STA. CRUZ DE TENERIFE", "Santa Cruz de Tenerife") %>% 
-  str_replace_all("LAS PALMAS", "Las Palmas")
-
-#Dataframe de datos_met lista para combianarse con otro dataframe
-
-
-
-#----------------
-# Pruebas
-
-for (i in 2000:2020){
-  print(i)
-  prueba <- as.data.frame(aemet_monthly_clim('6155A', year = i)) %>%
-    slice (1:12) %>%
-    select(c('ta_max', 'ta_min', 'tm_mes'))
-  Sys.sleep(1.5)
-}
-print(codigos)
-
-
-mal <-  c('C659H','3168D','9091R','1014A','6156X')
-bien <- c('C658L','3013','9091O','1014','6155A')
-remplazo <- c('C659H' = 'C658L', '3168D' = '3013','9091R'= '9091O', '1014A' = '1014', '6156X' = '6155A')
-
-codigos <- str_replace_all(codigos, remplazo)
-
-#----------------------------------------
-# DATOS DIABETES
-#----------------------------------------
-#obtiene los nombres de archivos
-archivos <- list.files('INPUT/DATA/Diabetes/Ingresos/', pattern = '*.px')
-#añade la ruta del archivo
-archivos_pc <- sapply(archivos, function(x) paste0('INPUT/DATA/Diabetes/Ingresos/',x))
-
-#crea un dataframe con el primer archivo que sevirá de molde para añadir los siguientes años
-df_i <- data.frame(as.data.frame(read.px(archivos_pc[1])))
-#añade el atributo del año de los datos
-df_i['Año'] = 1997
-
-for (i in 2:length(archivos_pc)){
-  #genera un df con los datos del año
-  df_provisional <- data.frame(as.data.frame(read.px(archivos_pc[i])))
-  
-  #obtiene el año a partir del nombre del archivo
-  año <- unlist(strsplit(archivos[i], "\\."))[1]
-  año <- substr(año,nchar(año)-3,nchar(año))
-  
-  #genera una columna con el año de los datos
-  df_provisional['Año'] = año
-  
-  #hace que los nombres de las columnas sean iguales al dataframe finak
-  colnames(df_provisional) <- colnames(df_i)
-  
-  #une el dataframe con los datos del año al df final
-  df_i <- rbind(df_i,df_provisional)
-}
 #----------------------------------------------------------------
 #Modificacion de dataframe df_i
 #----------------------------------------------------------------
@@ -624,4 +368,29 @@ grafico_muertes <- ggplot(df_m_filtrados, aes(x = reorder(Comunidad.Autonoma, -M
   theme_minimal()
 
 print(grafico_muertes) # Mostrar gráfico con ggplot
+
+# Gráfico adicional: Relación entre Temperatura Media y Ingresos por Diabetes
+# Preparamos los datos combinando los datasets
+datos_combinados <- datos_met_filtrados %>%
+  group_by(año) %>%
+  summarise(Temperatura_Media = mean(tm_mes, na.rm = TRUE)) %>%
+  inner_join(
+    df_i %>%
+      group_by(Año) %>%
+      summarise(Ingresos_Totales = sum(Ingresos, na.rm = TRUE)),
+    by = c("año" = "Año")
+  )
+
+# Creamos el gráfico
+grafico_relacion <- ggplot(datos_combinados, aes(x = Temperatura_Media, y = Ingresos_Totales)) +
+  geom_point(color = "blue", size = 3) +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  labs(
+    title = "Relación entre Temperatura Media y Ingresos por Diabetes",
+    x = "Temperatura Media (°C)",
+    y = "Ingresos Totales por Diabetes"
+  ) +
+  theme_minimal()
+
+print(grafico_relacion) # Mostrar gráfico con ggplot
 
